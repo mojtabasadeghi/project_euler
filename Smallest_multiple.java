120244186b870480e5d292e3b4e91de0c7b324e7
Adam <mojtaba.sadeghi1@gmail.com>
2022-12-22 15:08:40 -0800

    add Smallest multiple

---
+++ Smallest_multiple.java
@@ -0,0 +1,29 @@
+/**
+ *2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+ *What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ */
+public class Smallest_multiple {
+    public static void main(String[] args) {
+        int finalAnswer=1;
+        for (int i = 1; i < 20; i++) {
+            finalAnswer = lcm (finalAnswer, i+1);
+        }
+
+        System.out.println(finalAnswer);
+    }
+
+    public static int lcm(int number1, int number2) {
+        if (number1 == 0 || number2 == 0) {
+            return 0;
+        }
+        int absNumber1 = Math.abs(number1);
+        int absNumber2 = Math.abs(number2);
+        int absHigherNumber = Math.max(absNumber1, absNumber2);
+        int absLowerNumber = Math.min(absNumber1, absNumber2);
+        int lcm = absHigherNumber;
+        while (lcm % absLowerNumber != 0) {
+            lcm += absHigherNumber;
+        }
+        return lcm;
+    }
+}
